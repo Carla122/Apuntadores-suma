50afe6db706c3842f5968c6f6dfab82c726f5050
Ana Karen Escobar Casiano <ana_karita10@hotmail.com>
2018-08-22 07:56:45 -0500

    Agregue suma

---
+++ Suma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+    int arreglo[]={1,2,3,4,5,6,7,8,9,10};
+    int *Puntero;
+    int suma, promedio;
+int main(){
+    Puntero = &arreglo[0];
+    for(int i=0; i<10; i++){
+        cout << "Valor " << arreglo[i] << endl;
+        suma=suma+*Puntero++;
+    }
+    promedio = suma/10;
+    cout << "La suma es: " << suma <<endl;
+    cout << "El promedio es: " << promedio;
+
+    return 0;
+}
